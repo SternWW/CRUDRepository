717b0721698e4b56e6849fa4aa7e5be3db7b2a7d
WWuerthner <Warren@Warrens-MacBook-Pro.local>
2017-07-19 18:53:14 -0400

    Finished checkpoint one

--- app/products_app.py
+++ app/products_app.py
@@ -16,6 +16,22 @@ print("'Destroy' | Delete an existing product.")
 print("\n")
 function = input("Please input your chosen operation:  ")
 print("\n")
-print("You chose the " + function + " function! Great choice!")
+#print("You chose the " + function + " function! Great choice!")
 
 #Three quotes will give the ability to type a multi-line string into python directly (avoiding having to type all the pring and \n above) done with """
+
+if function.title() == "List": #using title function in case someone inputs 'list'
+    print("You chose the " + function + " function! Great choice!")
+elif function.title() == "Show":
+    print("You chose the " + function + " function! Great choice!")
+elif function.title() == "Create":
+    print("You chose the " + function + " function! Great choice!")
+elif function.title() == "Update":
+    print("You chose the " + function + " function! Great choice!")
+elif function.title() == "Destroy":
+    print("You chose the " + function + " function! Great choice!")
+else:
+    print("YOU HAVE CHOSEN AN Unrecognized Operation. Please choose one of: 'List', 'Show', 'Create', 'Update', or 'Destroy'.")
+
+
+print("\n")
